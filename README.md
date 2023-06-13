13cdc8cd93c0468eb758a48bc011488aeb73aecc
jakelipscomb <jlipscombtx95@gmail.com>
2023-06-12 23:54:57 -0500

    finished work on both html and js sides. html was tweaked with bootstrap elements and naming id conventions. js was reworked to align with content IDs and additional functionality with localStorage through the storeCity and addCityButton elements were crucial.

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# weather-dashboard
+# *module-6-challenge* (Weather Dashboard)
+
+## **Description**
+
+This project is a demonstration of a weather dashboard made with javascript with the help of a server-side API. The project utilizes dayjs for current date and time, localstorage for storing key value pairs when typing information in the boxes, and jQuery elements to change elements by adding and removing classes.
+
+## **Installation**
+
+https://jakelipscomb.github.io/weather-dashboard/
+
+![Weather Dashboard](/assets/images/weatherapp1.png)
+
+
+## **Technologies**
+Project is created with:
+* Visual Studio Code version: 1.78.2
+* JQuery
+* 
+	
+## **Setup**
+To run this project, clone it locally using bash:
+
+```
+$ bash
+$ git clone https://github.com/jakelipscomb/weather-dashboard.git
+```
+
+## **Credits**
+Coursework provided by The University of Texas at Austin Center for Professional Education Coding Boot Camp
+
+https://www.edx.org/course/the-coding-boot-camp-at-ut-austin#24775ad0-27b4-53da-943a-30a2a394c452
+
+## **License**
+
+MIT License
+
+Copyright (c) 2023 jakelipscomb
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,146 @@
-const apiKey = "8170b86792ecbebbf83218aa8a028564";
-var currentWeather = {};
-var weatherForecast = {};
+const apikey = "8170b86792ecbebbf83218aa8a028564";
+const today = dayjs();
+var cityInput = $("#city-input");
+var searchButton = $("#search-btn");
+var clearButton = $("#clear-btn");
 var city = "";
-var state = "";
-var icon = "";
-var temp = "";
-var wind = "";
-var humid = "";
-var location = [];
+var ico = "";
+var tem = "";
+var feel = "";
+var win = "";
+var hum = "";
+var locations = [];
 
-function fetchForecast() {
-    const forecastApi="https://api.openweathermap.org/data/2.5/forecast?q=" + city + ", " + state + "&appid=" + apiKey + "&units=imperial";
-    fetch(forecastApi)
-    .then(function(response) {
-        return response.json();
-    })
-}
+buildForecastCards();
 
 function fetchWeather() {
-    const weatherApi="https://api.openweathermap.org/data/2.5/weather?q=" + city + ", " + state + "&appid=" + apiKey + "&units=imperial";
-    fetch(weatherApi)
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apikey + "&units=imperial";
+    fetch(requestURL)
     .then(function(response) {
-        return response.json();
+        if (response.status === 404) {
+            console.log("404");
+            return;
+        } else {
+            return response.json();
+        }
     })
     .then(function(data) {
-        icon = data.weather[0].icon;
-        temp = data.main.temp;
-        wind = data.wind.speed;
-        humid = data.main.humidity;
+        ico = data.weather[0].icon;
+        tem = data.main.temp;
+        feel = data.main.feels_like;
+        win = data.wind.speed;
+        hum = data.main.humidity;
+        popCurrent();
+        popForecast();
+        buildForecastCards();
+    })
+    // .catch(function(error) {
+    //     console.log(error);
+    // });
+}
+
+function popCurrent() {
+    var currentDate = today.format("dddd, MMM D, YYYY");
+    $("#current-city").text(city + " " + currentDate);
+    $("#current-icon").attr("src", 'https://openweathermap.org/img/wn/' + ico + '@2x.png');
+    $("#current-temp").text("Temp: " + tem);
+    $("#current-feelslike").text("Feels like: " + feel);
+    $("#current-wind").text("Wind: " + win + " MPH");
+    $("#current-humid").text("Humidity: " + hum + "%");
+}
+
+function popForecast() {
+    var requestFutureURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=" + apikey + "&units=imperial";
+    fetch(requestFutureURL)
+    .then(function(response) {
+        if (response.status === 404) {
+            console.log("404");
+            return;
+        } else {
+            return response.json();
+        }
     })
     .then(function(data) {
-        currentWeather();
-        forecastWeather();
+        var ind = 1;
+        for (let i = 0; i < data.list.length; i += 8) {
+            var futureDate = today.add(ind, "d").format("dddd, MMM D, YYYY");
+            $("#fut-date-" + ind).text(futureDate);
+            $("#fut-icon-" + ind).attr("src", 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png');
+            $("#fut-temp-" + ind).text("Temp: " + data.list[i].main.temp);
+            $("#fut-feelslike-" + ind).text("Feels Like: " + data.list[i].main.feels_like);
+            $("#fut-wind-" + ind).text("Wind: " + data.list[i].wind.speed);
+            $("#fut-humid-" + ind).text("Humidity: " + data.list[i].main.humidity);
+            ind++;
+        }
     })
+    .catch(function(error) {
+        console.log(error);
+    });
 }
 
-function currentWeather() {
-    const currentDay = dayjs().format("dddd, MMM D, YYYY");
-    $("")
+function buildForecastCards() {
+    $("#forecast-container").empty();
+    for (i = 1; i <= 5; i++) {
+        var futureDate = today.add(i, "d").format("dddd, MMM D, YYYY");
+        var forecastCard = "";
+        forecastCard += '<div class="card col-2 bg-dark text-light align-items-center my-1">'
+        forecastCard += '<p id="fut-date-' + i + '">' + futureDate + '</p>';
+        forecastCard += '<p><img class="img-fluid" id="fut-icon-' + i + '" src=""></p>';
+        forecastCard += '<p id="fut-temp-' + i + '"></p>';
+        forecastCard += '<p id="fut-feelslike-' + i + '"></p>';
+        forecastCard += '<p id="fut-wind-' + i + '"></p>';
+        forecastCard += '<p id="fut-humid-' + i + '"></p>';
+        forecastCard += "</div>"
+        $("#forecast-container").append(forecastCard);
+    }
 }
 
-function forecastWeather() {
-    const currentDay = dayjs().format("dddd, MMM D, YYYY");
-    $("")
+function clearHistory() {
+    localStorage.clear();
+    locations = [];
+    $("#prev-city-container").empty();
 }
+
+function addCityButton() {
+    var storedLocations = JSON.parse(localStorage.getItem("locations"));
+
+    $("#prev-city-container").empty();
+    if (storedLocations !== null) {
+        locations = storedLocations;
+        for (let i = 0; i < locations.length; i++) {
+            var cityButton = '<button id="' + locations[i].city + '" class="btn btn-success p-1 my-2 w-100 cityBtn">' + locations[i].city + '</button>';
+            $("#prev-city-container").append(cityButton);
+        }
+    }
+
+    $(".cityBtn").on("click", function(event) {
+        event.preventDefault();
+        city = $(this).attr("id");
+        fetchWeather();
+    });
+}
+
+function storeCity(cityName) {
+    var newCity = {
+        city: cityName.trim(),
+    };
+    locations.push(newCity);
+    localStorage.setItem("locations", JSON.stringify(locations));
+    addCityButton();
+}
+
+searchButton.on("click", function(event) {
+    event.preventDefault();
+    city = cityInput.val().trim();
+    if (city === "") {
+        return;
+    }
+    fetchWeather();
+    cityInput.val("");
+    storeCity(city);
+});
+
+clearButton.on("click", function(event) {
+    event.preventDefault();
+    clearHistory();
+});
